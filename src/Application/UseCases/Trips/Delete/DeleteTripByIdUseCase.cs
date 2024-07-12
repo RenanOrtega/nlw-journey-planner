bc7eb35976ac471c67dd95e890750cb6ad728c0a
Renan Johny Ortega <renanortega82@gmail.com>
2024-07-12 01:27:20 -0300

    feat: create use cases.
    Signed-off-by: Renan Johny Ortega <renanortega82@gmail.com>

---
+++ src/Application/UseCases/Trips/Delete/DeleteTripByIdUseCase.cs
@@ -0,0 +1,26 @@
+﻿using Exception.ExceptionsBase;
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Trips.Delete;
+
+public class DeleteTripByIdUseCase
+{
+    public void Execute(Guid id)
+    {
+        var dbContext = new JourneyDbContext();
+
+        var trip = dbContext
+            .Trips
+            .Include(trip => trip.Activities)
+            .FirstOrDefault(trip => trip.Id == id);
+
+        if (trip is null)
+        {
+            throw new NotFoundException(ResourceErrorMessages.TRIP_NOT_FOUND);
+        }
+
+        dbContext.Trips.Remove(trip);
+        dbContext.SaveChanges();
+    }
+}
---
+++ src/Application/UseCases/Trips/GetById/GetTripByIdUseCase.cs
@@ -0,0 +1,39 @@
+﻿using Communication.Responses;
+using Exception.ExceptionsBase;
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Trips.GetById;
+
+public class GetTripByIdUseCase
+{
+    public ResponseTripJson Execute(Guid id)
+    {
+        var dbContext = new JourneyDbContext();
+
+        var trip = dbContext
+            .Trips
+            .Include(trip => trip.Activities)
+            .FirstOrDefault(trip => trip.Id == id);
+
+        if (trip is null)
+        {
+            throw new NotFoundException(ResourceErrorMessages.TRIP_NOT_FOUND);
+        }
+
+        return new ResponseTripJson
+        {
+            Id = trip.Id,
+            EndDate = trip.EndDate,
+            StartDate = trip.StartDate,
+            Name = trip.Name,
+            Activities = trip.Activities.Select(activity => new ResponseActivityJson
+            {
+                Name = activity.Name,
+                Id = activity.Id,
+                Date = activity.Date,
+                Status = (Communication.Enums.ActivityStatus)activity.Status
+            }).ToList()
+        };
+    }
+}

dab6d3ffe892cc90a39004385a076ceb19b4f0c4
Renan Johny Ortega <renanortega82@gmail.com>
2024-07-13 02:12:31 -0300

    fix: set status to Pending when create an activity.
    Signed-off-by: Renan Johny Ortega <renanortega82@gmail.com>

--- src/Infrastructure/Entities/Activity.cs
+++ src/Infrastructure/Entities/Activity.cs
@@ -1,11 +1,11 @@
 ﻿using Journey.Infrastructure.Enums;
 
-namespace Journey.Infrastructure.Entities;
+namespace Infrastructure.Entities;
 public class Activity
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = string.Empty;
     public DateTime Date { get; set; }
-    public ActivityStatus Status { get; set; }
+    public ActivityStatus Status { get; set; } = ActivityStatus.Pending;
     public Guid TripId { get; set; }
 }
